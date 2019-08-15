79056ea122ed8b235791cc344cfe6a17a153e3a5
reginPekin <reginbegin@gmail.com>
2019-08-15 22:34:00 +0300

    App

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,19 +1,19 @@
-// import React from "react";
-// import { connect } from "react-redux";
+import React from "react";
+import { connect } from "react-redux";
 
-// import { FolderWindow } from "../FolderWindow";
+import { FolderWindow } from "../FolderWindow";
 
-// export const App = ({ vocabulary }) => {
-//   return (
-//     <div>
-//       {vocabulary.map((folder, key) => (
-//         <FolderWindow key={key} folder={folder} />
-//       ))}
-//     </div>
-//   );
-// };
+export const App = ({ vocabulary }) => {
+  return (
+    <div>
+      {vocabulary.map((folder, key) => (
+        <FolderWindow key={key} folder={folder} />
+      ))}
+    </div>
+  );
+};
 
-// const mapStateToProps = state => ({
-//   vocabulary: state.showPage.array
-// });
-// export const MethodsList = connect(mapStateToProps)(App);
+const mapStateToProps = state => ({
+  vocabulary: state.showPage.array
+});
+export const MethodsList = connect(mapStateToProps)(App);
