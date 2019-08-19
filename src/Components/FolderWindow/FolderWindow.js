ec9256a6a45090089a01c7eefa63fa0d07426a22
reginPekin <reginbegin@gmail.com>
2019-08-19 04:37:30 +0300

    added folderSearch and redux connect

--- src/Components/FolderWindow/FolderWindow.js
+++ src/Components/FolderWindow/FolderWindow.js
@@ -1,13 +1,30 @@
 import React from "react";
+import { connect } from "react-redux";
 
 import { FolderBox } from "../FolderBox";
+import { FolderSearch } from "../FolderSearch";
 
-export const FolderWindow = ({ vocabulary }) => {
+import styles from "./FolderWindow.module.css";
+
+const FolderWindowContainer = ({ vocabulary, searchText }) => {
   return (
-    <div>
-      {vocabulary.map((folder, key) => (
-        <FolderBox folder={folder} key={key} />
-      ))}
+    <div className={styles.folderWindow}>
+      <FolderSearch />
+      {vocabulary.map(
+        (folder, key) =>
+          (searchText === "" ||
+            folder.folderName
+              .toLowerCase()
+              .indexOf(searchText.toLowerCase()) !== -1) && (
+            <FolderBox folder={folder} key={key} />
+          )
+      )}
     </div>
   );
 };
+
+const mapStateProps = state => ({
+  searchText: state.smallActions.searchText
+});
+
+export const FolderWindow = connect(mapStateProps)(FolderWindowContainer);
