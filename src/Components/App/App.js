9ee506ca8c9b2b9be8081614d656df3884c79659
reginPekin <reginbegin@gmail.com>
2019-08-20 22:59:56 +0300

    deleted redux

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,21 +1,15 @@
 import React from "react";
-import { connect } from "react-redux";
 
 import styles from "./App.module.css";
 
 import { FolderWindow } from "../FolderWindow";
 import { VocabularyWindow } from "../VocabularyWindow";
 
-export const AppContainer = ({ Vocabulary }) => {
+export const App = () => {
   return (
     <div className={styles.app}>
-      <FolderWindow vocabulary={Vocabulary} />
+      <FolderWindow />
       <VocabularyWindow />
     </div>
   );
 };
-
-const mapStateToProps = state => ({
-  Vocabulary: state.showPage.array
-});
-export const App = connect(mapStateToProps)(AppContainer);
