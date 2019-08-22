9b1a44c2e4a64d2be4800be396855856f9d2e898
reginPekin <reginbegin@gmail.com>
2019-08-22 20:11:14 +0300

    all folders is open in the beggining

--- src/Vocabulary.js
+++ src/Vocabulary.js
@@ -2,7 +2,7 @@ export const Vocabulary = [
   {
     folderId: 0,
     folderName: "Animals",
-    isOpen: false,
+    isOpen: true,
     words: [
       { wordId: 0, foreignWord: "mouse", nativeWord: "мышь" },
       { wordId: 1, foreignWord: "snake", nativeWord: "змея" },
@@ -12,7 +12,7 @@ export const Vocabulary = [
   {
     folderId: 1,
     folderName: "Plants",
-    isOpen: false,
+    isOpen: true,
     words: [
       { wordId: 0, foreignWord: "pine", nativeWord: "сосна" },
       { wordId: 1, foreignWord: "tulpin", nativeWord: "тюльпан" },
