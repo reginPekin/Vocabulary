428e36ad9e6ed0e811f021c572ad9afe46eb656c
reginPekin <reginbegin@gmail.com>
2019-08-22 18:07:10 +0300

    deleted class

--- src/Components/FolderBox/FolderBox.js
+++ src/Components/FolderBox/FolderBox.js
@@ -1,10 +1,8 @@
 import React from "react";
 
-import styles from "./FolderBox.module.css";
-
 export const FolderBox = ({ folder }) => {
   return (
-    <div className={styles.folderBox}>
+    <div>
       <button>{folder.folderName}</button>
     </div>
   );
