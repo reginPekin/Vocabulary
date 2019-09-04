69a8014728a25c2bee1d9faf993c6f9f77a882fc
reginPekin <reginbegin@gmail.com>
2019-09-04 23:41:39 +0300

    show words of folder

--- src/Components/VocabularyWindow/VocabularyWindow.js
+++ src/Components/VocabularyWindow/VocabularyWindow.js
@@ -2,17 +2,31 @@ import React from "react";
 import { connect } from "react-redux";
 
 import { VocabularyTable } from "../VocabularyTable";
+import { AddWord } from "../AddWord";
+
+import style from "./VocabularyWindow.module.css";
 // import styles from "./VocabularyWindow.module.css";
 
 const VocabularyWindowContainer = ({ vocabulary, history }) => {
-  console.log("asdasdas", history.location.pathname);
-  return (
-    <div>
-      {vocabulary.map((folder, key) => (
-        <VocabularyTable folder={folder} key={key} />
-      ))}
-    </div>
-  );
+  const index = history.location.pathname.indexOf("/", 2);
+  const id = history.location.pathname.slice(1, index);
+
+  if (id.length > 0) {
+    return (
+      <div className={style.vocabularyWindow}>
+        <VocabularyTable folder={vocabulary[id]} key={id} />
+        <AddWord folderId={id} />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        {vocabulary.map((folder, key) => (
+          <VocabularyTable folder={folder} key={key} />
+        ))}
+      </div>
+    );
+  }
 };
 
 const mapStateProps = state => ({
