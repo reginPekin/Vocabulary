7cb98f3df1b32cc069ea923841231d2ba3507e17
reginPekin <reginbegin@gmail.com>
2019-09-04 23:38:18 +0300

    css padding

--- src/Components/DisplayPair/DisplayPair.js
+++ src/Components/DisplayPair/DisplayPair.js
@@ -1,8 +1,10 @@
 import React from "react";
 
+import style from "./DisplayPair.module.css";
+
 export const DisplayPair = ({ wordPair }) => {
   return (
-    <div>
+    <div className={style.displayPair}>
       <span>{wordPair.foreignWord}: </span>
       <span>{wordPair.nativeWord}</span>
     </div>
