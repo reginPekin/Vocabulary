1925d1308960e99a2b3d46fe95d63fa9b5ab21d0
reginPekin <reginbegin@gmail.com>
2019-09-04 23:38:35 +0300

    css

--- src/Components/FolderBox/FolderBox.js
+++ src/Components/FolderBox/FolderBox.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import style from "./FolderBox.module.css";
+
 export const FolderBox = ({ folder }) => {
   return (
     <Link to={`/${folder.folderId}/${folder.folderName}`}>
-      <button>{folder.folderName}</button>
+      <button className={style.boxFolder}>{folder.folderName}</button>
     </Link>
   );
 };
