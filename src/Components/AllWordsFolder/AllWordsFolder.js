298ecca947ad6ff4134fb980e03cb40cb6003f55
reginPekin <reginbegin@gmail.com>
2019-09-04 23:38:08 +0300

    css padding

--- src/Components/AllWordsFolder/AllWordsFolder.js
+++ src/Components/AllWordsFolder/AllWordsFolder.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import style from "./AllWordsFolder.module.css";
+
 export const AllWordsFolder = () => {
   return (
     <Link to="/">
-      <button>full voc</button>
+      <button className={style.allWordsButton}>All words</button>
     </Link>
   );
 };
