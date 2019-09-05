7e01d1ecb91dfb8904f9499226fbedce3e77356c
reginPekin <reginbegin@gmail.com>
2019-09-05 23:33:31 +0300

    connect with another css

--- src/Components/AllWordsFolder/AllWordsFolder.js
+++ src/Components/AllWordsFolder/AllWordsFolder.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import style from "./AllWordsFolder.module.css";
+import style from "../FolderBox/FolderBox.module.css";
 
 export const AllWordsFolder = () => {
   return (
     <Link to="/">
-      <button className={style.allWordsButton}>All words</button>
+      <button className={style.boxFolder}>All words</button>
     </Link>
   );
 };
