73f58b38042fd9a58a80ffae887e9bf2d2e2181d
reginPekin <reginbegin@gmail.com>
2019-09-11 23:41:26 +0300

    table raws

--- src/Components/Vocabulary/Vocabulary.js
+++ src/Components/Vocabulary/Vocabulary.js
@@ -3,11 +3,24 @@ import React from "react";
 import { VocabularyTable } from "../VocabularyTable";
 import { AddWord } from "../AddWord";
 
+import style from "./Vocabulary.module.css";
+
 export const Vocabulary = ({ folder, folderId }) => {
   return (
-    <div>
-      <VocabularyTable folder={folder} key={folderId} />
-      {folderId >= 0 && <AddWord folderId={folderId} />}
-    </div>
+    <table>
+      <tbody>
+        <VocabularyTable folder={folder} />
+        {folderId >= 0 && (
+          <tr className={style.addWords}>
+            <td>
+              <AddWord intFolderId={parseInt(folderId, 10)} word="foreign" />
+            </td>
+            <td>
+              <AddWord intFolderId={parseInt(folderId, 10)} word="native" />
+            </td>
+          </tr>
+        )}
+      </tbody>
+    </table>
   );
 };
