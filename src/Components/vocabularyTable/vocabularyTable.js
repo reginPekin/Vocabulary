44a95a95f77003eb53ce21f78de5c442bff3aac2
reginPekin <reginbegin@gmail.com>
2019-09-12 14:46:13 +0300

    rename

--- src/Components/vocabularyTable/vocabularyTable.js
+++ src/Components/vocabularyTable/vocabularyTable.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-import { DisplayPair } from "../DisplayPair";
+import { PairOfWords } from "../PairOfWords";
 
 // import style from "./VocabularyTable.module.css";
 
 export const VocabularyTable = ({ folder }) => {
   return folder.words.map((wordPair, key) => (
-    <DisplayPair wordPair={wordPair} key={key} />
+    <PairOfWords wordPair={wordPair} key={key} />
   ));
 };
