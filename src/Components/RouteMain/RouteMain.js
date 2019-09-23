a6a14dad5a0392bf99d434fcf5f2907991850eca
reginPekin <reginbegin@gmail.com>
2019-09-23 18:17:59 +0300

    small change

--- src/Components/RouteMain/RouteMain.js
+++ src/Components/RouteMain/RouteMain.js
@@ -5,11 +5,13 @@ import { VocabularyWindow } from "../VocabularyWindow";
 
 import style from "./RouteMain.module.css";
 
-export const RouteMain = () => (
-  <main className={style.main}>
-    <Switch>
-      <Route exact path="/" component={VocabularyWindow} />
-      <Route path="/:id" render={props => <VocabularyWindow {...props} />} />
-    </Switch>
-  </main>
-);
+export const RouteMain = () => {
+  return (
+    <main className={style.main}>
+      <Switch>
+        <Route exact path="/" component={VocabularyWindow} />
+        <Route path="/:id" render={props => <VocabularyWindow {...props} />} />
+      </Switch>
+    </main>
+  );
+};
