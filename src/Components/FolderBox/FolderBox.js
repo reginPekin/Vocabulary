c42ff3abe07fbb9a3e997426f841296b1c31e505
reginPekin <reginbegin@gmail.com>
2019-09-23 18:16:17 +0300

    delete fold from db

--- src/Components/FolderBox/FolderBox.js
+++ src/Components/FolderBox/FolderBox.js
@@ -1,12 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 import style from "./FolderBox.module.css";
 
-export const FolderBox = ({ folder }) => {
+export const FolderBox = ({ folder, reset, onDispatch }) => {
   return (
-    <Link to={`/${folder.folderId}/${folder.folderName}`}>
-      <button className={style.boxFolder}>{folder.folderName}</button>
-    </Link>
+    <div>
+      <Link to={`/${folder.folderName}/${folder.folderId}`}>
+        <button className={style.boxFolder}>{folder.folderName}</button>
+      </Link>
+      <button
+        onClick={() => {
+          axios
+            .get("http://localhost:4000/vocabulary/delete/" + folder._id)
+            .then(() => reset())
+            .catch(err => console.log(err));
+          onDispatch();
+        }}
+      >
+        Delete me :c
+      </button>
+    </div>
   );
 };
