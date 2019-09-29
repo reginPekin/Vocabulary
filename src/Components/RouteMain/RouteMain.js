77033f8679ec062263bb72887393b449caf2f8fc
reginPekin <reginbegin@gmail.com>
2019-09-29 18:40:16 +0300

    deleted pafe with all words

--- src/Components/RouteMain/RouteMain.js
+++ src/Components/RouteMain/RouteMain.js
@@ -2,6 +2,7 @@ import { Switch, Route } from "react-router-dom";
 import React from "react";
 
 import { VocabularyWindow } from "../VocabularyWindow";
+import { Hello } from "../Hello";
 
 import style from "./RouteMain.module.css";
 
@@ -9,7 +10,7 @@ export const RouteMain = () => {
   return (
     <main className={style.main}>
       <Switch>
-        <Route exact path="/" component={VocabularyWindow} />
+        <Route exact path="/" component={Hello} />
         <Route path="/:id" render={props => <VocabularyWindow {...props} />} />
       </Switch>
     </main>
