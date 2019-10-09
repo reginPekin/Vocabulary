c99ed3dfe350ed299c9b2759723b1f773598293c
reginPekin <reginbegin@gmail.com>
2019-10-09 16:52:06 +0300

    renamed getting folder + catch errors

--- src/Components/Vocabulary/Vocabulary.js
+++ src/Components/Vocabulary/Vocabulary.js
@@ -11,13 +11,18 @@ import style from "./Vocabulary.module.css";
 const VocabularyContainer = ({ dispatch, folder }) => {
   const [reset, setReset] = useState(0);
   useEffect(() => {
-    axios.get("http://localhost:4000/vocabulary/" + folder._id).then(response =>
-      dispatch({
-        type: "ADD_WORDS_ARRAY",
-        folderId: folder.folderId,
-        words: response.data
-      })
-    );
+    axios
+      .get("http://localhost:4000/vocabulary/folderVocabulary/" + folder._id)
+      .then(response =>
+        dispatch({
+          type: "ADD_WORDS_ARRAY",
+          folderId: folder.folderId,
+          words: response.data
+        })
+      )
+      .catch(error => {
+        console.log(error);
+      });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [dispatch, reset]);
 
