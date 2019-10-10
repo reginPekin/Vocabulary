b3a1f696b5375fdee0836fee8aec370a4e10a906
reginPekin <reginbegin@gmail.com>
2019-10-10 22:50:32 +0300

    redux in folderBox

--- src/Components/FolderBox/FolderBox.js
+++ src/Components/FolderBox/FolderBox.js
@@ -2,20 +2,38 @@ import React from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
+import { connect } from "react-redux";
+
 import style from "./FolderBox.module.css";
 
-export const FolderBox = ({ folder, reset }) => {
-  // console.log(folder);
+const FolderBoxContainer = ({ folder, dispatch, wordCounter }) => {
+  // console.log(wordCounter);
   return (
     <div>
       <Link to={`/${folder.folderId}/${folder.folderName}`}>
-        <button className={style.boxFolder}>{folder.folderName}</button>
+        <button
+          onClick={() => {
+            if (folder.words === undefined) {
+              dispatch({
+                type: "INCREASE_WORD_COUNTER",
+                wordCounter: wordCounter
+              });
+            }
+          }}
+          className={style.boxFolder}
+        >
+          {folder.folderName}
+        </button>
       </Link>
       <button
         onClick={() => {
           axios
-            .get("http://localhost:4000/vocabulary/delete/" + folder._id)
-            .then(() => reset())
+            .delete(
+              "http://localhost:4000/vocabulary/folders/" + folder.folderId
+            )
+            .then(
+              dispatch({ type: "DELETE_FOLDER", folderId: folder.folderId })
+            )
             .catch(err => console.log(err));
         }}
       >
@@ -24,3 +42,9 @@ export const FolderBox = ({ folder, reset }) => {
     </div>
   );
 };
+
+const mapStateProps = state => ({
+  wordCounter: state.smallActions.wordCounter
+});
+
+export const FolderBox = connect(mapStateProps)(FolderBoxContainer);
