3eae82bec7434bf89891b6bd79e5c65c01db56fb
reginPekin <reginbegin@gmail.com>
2019-10-11 22:59:05 +0300

    redux2 -> redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { store } from "./redux2";
+import { store } from "./redux";
 import { BrowserRouter } from "react-router-dom";
 
 import "./style/index.css";
