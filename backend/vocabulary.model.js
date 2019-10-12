22be8d298333c8830e8154f9da3e0f318a140903
reginPekin <reginbegin@gmail.com>
2019-10-12 23:12:29 +0300

    delete __v and add date

--- backend/vocabulary.model.js
+++ backend/vocabulary.model.js
@@ -1,16 +1,22 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-let Vocabulary = new Schema({
-  folderName: {
-    type: String
+let Vocabulary = new Schema(
+  {
+    folderName: {
+      type: String
+    },
+    folderId: {
+      type: Number
+    },
+    words: {
+      type: Array
+    },
+    date: {
+      type: Number
+    }
   },
-  folderId: {
-    type: Number
-  },
-  words: {
-    type: Array
-  }
-});
+  { versionKey: false }
+);
 
 module.exports = mongoose.model("Vocabulary", Vocabulary);
