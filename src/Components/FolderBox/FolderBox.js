9fe242075c4cd0c49fe1125d7f893f6aa6902fee
reginPekin <reginbegin@gmail.com>
2019-10-14 23:25:56 +0300

    different diplay edit and folder name

--- src/Components/FolderBox/FolderBox.js
+++ src/Components/FolderBox/FolderBox.js
@@ -8,65 +8,70 @@ import style from "./FolderBox.module.css";
 
 const FolderBoxContainer = ({ folder, dispatch, wordCounter }) => {
   const [text, setText] = useState("");
+  const [visibility, setVisibility] = useState(true);
   return (
     <div>
-      <div className={style.buttonsDiv}>
-        <Link to={`/${folder.folderId}/${folder.folderName}`}>
+      {visibility && (
+        <div className={style.buttonsDiv}>
+          <Link to={`/${folder.folderId}/${folder.folderName}`}>
+            <button
+              onClick={() => {
+                if (folder.words === undefined) {
+                  dispatch({
+                    type: "INCREASE_WORD_COUNTER",
+                    wordCounter: wordCounter
+                  });
+                }
+              }}
+              className={style.boxFolder}
+            >
+              {folder.folderName}
+            </button>
+          </Link>
+          <button onClick={() => setVisibility(!visibility)}>Edit me!</button>
           <button
             onClick={() => {
-              if (folder.words === undefined) {
-                dispatch({
-                  type: "INCREASE_WORD_COUNTER",
-                  wordCounter: wordCounter
-                });
-              }
+              axios
+                .delete(
+                  "http://localhost:4000/vocabulary/folders/" + folder.folderId
+                )
+                .then(
+                  dispatch({ type: "DELETE_FOLDER", folderId: folder.folderId })
+                )
+                .catch(err => console.log(err));
             }}
-            className={style.boxFolder}
           >
-            {folder.folderName}
+            Delete me :c
           </button>
-        </Link>
-        <button>Edit me!</button>
-        <button
-          onClick={() => {
-            axios
-              .delete(
-                "http://localhost:4000/vocabulary/folders/" + folder.folderId
-              )
-              .then(
-                dispatch({ type: "DELETE_FOLDER", folderId: folder.folderId })
-              )
-              .catch(err => console.log(err));
+        </div>
+      )}
+      {!visibility && (
+        <form
+          onSubmit={event => {
+            axios.patch(
+              "http://localhost:4000/vocabulary/folders/" + folder.folderId,
+              { folderName: text }
+            );
+            dispatch({
+              type: "RENAME_FOLDER",
+              folderId: folder.folderId,
+              folderName: text
+            });
+            setText("");
+            setVisibility(!visibility);
+            event.preventDefault();
           }}
         >
-          Delete me :c
-        </button>
-      </div>
-
-      <form
-        onSubmit={event => {
-          axios.patch(
-            "http://localhost:4000/vocabulary/folders/" + folder.folderId,
-            { folderName: text }
-          );
-          dispatch({
-            type: "RENAME_FOLDER",
-            folderId: folder.folderId,
-            folderName: text
-          });
-          setText("");
-          event.preventDefault();
-        }}
-      >
-        <input
-          type="text"
-          placeholder="new folder"
-          value={text}
-          onChange={event => {
-            setText(event.target.value);
-          }}
-        />
-      </form>
+          <input
+            type="text"
+            placeholder="new folder"
+            value={text}
+            onChange={event => {
+              setText(event.target.value);
+            }}
+          />
+        </form>
+      )}
     </div>
   );
 };
