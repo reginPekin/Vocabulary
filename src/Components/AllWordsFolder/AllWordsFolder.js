29d77fba20d3ad38c3afe8a411120ad1b86ccc59
reginPekin <reginbegin@gmail.com>
2019-10-24 00:26:14 +0300

    change backgroundColor in startPage

--- src/Components/AllWordsFolder/AllWordsFolder.js
+++ src/Components/AllWordsFolder/AllWordsFolder.js
@@ -3,10 +3,17 @@ import { Link } from "react-router-dom";
 
 import style from "../FolderBox/FolderBox.module.css";
 
-export const AllWordsFolder = () => {
+export const AllWordsFolder = ({ currentFolderId }) => {
   return (
     <Link to="/">
-      <button className={style.boxFolder}>Start</button>
+      <button
+        className={style.boxFolder}
+        style={{
+          backgroundColor: currentFolderId === "" ? "#D4D4D4" : "white"
+        }}
+      >
+        Start
+      </button>
     </Link>
   );
 };
