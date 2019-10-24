1ee2ea9e5697c665d7d33cc9a941fd606fc9d98a
reginPekin <reginbegin@gmail.com>
2019-10-24 19:17:41 +0300

    isSuitable

--- src/utils/smallActions.js
+++ src/utils/smallActions.js
@@ -1,13 +1,21 @@
-export const increaseWordCounter = wordCounter => {
-  return {
-    type: "INCREASE_WORD_COUNTER",
-    wordCounter
-  };
-};
-
 export const setSearchText = searchText => {
   return {
     type: "CHANGE_SEARCH_TEXT",
     searchText
   };
+};
+
+export const getCurrentFolderId = history => {
+  const currentFolderLink = history.location.pathname;
+
+  return currentFolderLink.slice(1, currentFolderLink.indexOf("/", 2));
+};
+
+export const isSuitable = (searchText, folder) => {
+  if (
+    !searchText ||
+    folder.name.toLowerCase().indexOf(searchText.toLowerCase()) !== -1
+  )
+    return true;
+  else return false;
 };
