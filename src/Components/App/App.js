9d11bbbedb9740e6b127b093c29465385c0f0ee2
reginPekin <reginbegin@gmail.com>
2019-10-24 19:14:38 +0300

    renamed FolderWindow + MainRouter

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,15 +1,20 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 
 import styles from "./App.module.css";
 
-import { FolderWindow } from "../FolderWindow";
-import { RouteMain } from "../RouteMain";
+import { Menu } from "../Menu";
+import { VocabularyWindow } from "../VocabularyWindow";
+import { Hello } from "../Hello";
 
-export const App = () => {
-  return (
-    <div className={styles.app}>
-      <FolderWindow />
-      <RouteMain />
-    </div>
-  );
-};
+export const App = () => (
+  <main className={styles.app}>
+    <Menu />
+    <section className={styles.main}>
+      <Switch>
+        <Route exact path="/" component={Hello} />
+        <Route path="/:id" render={props => <VocabularyWindow {...props} />} />
+      </Switch>
+    </section>
+  </main>
+);
