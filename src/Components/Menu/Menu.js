df1ca9223392a00c59c1a0faabb4600c145fb365
reginPekin <reginbegin@gmail.com>
2019-10-28 23:56:17 +0300

    not found boundary

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,10 +1,11 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
-import { withRouter } from "react-router";
+import { NotFoundBoundary, useLoadingRoute, useCurrentRoute } from "react-navi";
 
 import { FolderBox } from "../FolderBox";
 import { FolderSearch } from "../FolderSearch";
 import { NewFolder } from "../NewFolder";
+import { LoadingBar } from "../LoadingBar";
 
 import { getFoldersNames } from "../../utils/folderUtils";
 import { getCurrentFolderId, isSuitable } from "../../utils/smallActions";
@@ -13,15 +14,21 @@ import * as sdk from "../../sdk";
 
 import styles from "./Menu.module.css";
 
-const MenuContainerRouter = ({ dispatch, searchText, folders, history }) => {
-  const currentFolderId = getCurrentFolderId(history);
+const MenuContainerRouter = ({ children, dispatch, searchText, folders }) => {
+  let loadingRoute = useLoadingRoute();
+  let route = useCurrentRoute();
+  const currentFolderId = getCurrentFolderId(route.url.pathname);
 
   useEffect(() => {
-    sdk.getFolderNames().then(response => dispatch(getFoldersNames(response)));
+    sdk.getFolderNames().then(response => {
+      console.log("response", response);
+      dispatch(getFoldersNames(response));
+    });
   }, [dispatch]);
 
   return (
     <div className={styles.folderWindow}>
+      <LoadingBar isActive={!!loadingRoute} />
       {folders.length > 5 && <FolderSearch />}
 
       {folders.map(
@@ -34,11 +41,23 @@ const MenuContainerRouter = ({ dispatch, searchText, folders, history }) => {
             />
           )
       )}
-
-      <NewFolder history={history} />
+      <NewFolder />
+      <main>
+        <NotFoundBoundary render={renderNotFound}>
+          {children || null}
+        </NotFoundBoundary>
+      </main>
     </div>
   );
 };
+
+function renderNotFound() {
+  return (
+    <div className="App-error">
+      <h1>404 - Not Found</h1>
+    </div>
+  );
+}
 
 const mapStateProps = state => ({
   searchText: state.smallActions.searchText,
@@ -47,4 +66,4 @@ const mapStateProps = state => ({
 
 const MenuRouter = connect(mapStateProps)(MenuContainerRouter);
 
-export const Menu = withRouter(MenuRouter);
+export const Menu = MenuRouter;
