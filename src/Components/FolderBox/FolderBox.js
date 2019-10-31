097e7832acba0451b966830a5d28f5e98b8f28cd
reginPekin <reginbegin@gmail.com>
2019-10-31 21:54:39 +0300

    нет edit folder, но осталось их удаление пока

--- src/Components/FolderBox/FolderBox.js
+++ src/Components/FolderBox/FolderBox.js
@@ -1,128 +1,41 @@
-import React, { useState, useRef, useEffect } from "react";
+import React from "react";
 import { Link } from "react-navi";
-
-import { useDispatch } from "react-redux";
-
-import { deleteFolder, renameFolder } from "../../utils/folderUtils";
 
 import * as sdk from "../../sdk";
 
-import style from "./FolderBox.module.css";
+import styles from "./FolderBox.module.css";
 
-const FolderButton = ({
-  folder,
-  dispatch,
-  changeVisibility,
-  currentFolderId
-}) => {
+const FolderButton = ({ folder, isActive, onDelete }) => {
   return (
-    <div className={style.buttonsDiv}>
+    <div>
       <Link href={`/voc/${folder.id}`} activeClassName="active">
         <button
           style={{
-            backgroundColor:
-              parseInt(currentFolderId) === folder.id ? "#D4D4D4" : "white"
+            border:
+              parseInt(isActive) === folder.id
+                ? "2px solid #808080"
+                : "2px solid white"
           }}
-          className={style.boxFolder}
+          className={styles.boxFolder}
         >
           {folder.name}
         </button>
       </Link>
-      <button onClick={() => changeVisibility()}>Edit me!</button>
       <Link href="/" activeClassName="active">
         <button
           onClick={() => {
-            sdk.deleteFolder(folder).then(dispatch(deleteFolder(folder.id)));
+            sdk.deleteFolder(folder.id).then(() => onDelete(folder.id));
           }}
         >
-          Delete me :c
+          X
         </button>
       </Link>
     </div>
   );
 };
 
-const FolderInput = ({ folder, dispatch, changeVisibility }) => {
-  const [text, setText] = useState(folder.name);
-
-  const inputRename = useRef(null);
-
-  // Listener on Click Outside
-  useEffect(() => {
-    const handleClickOutside = event => {
-      if (inputRename.current && !inputRename.current.contains(event.target)) {
-        changeVisibility();
-      }
-    };
-
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => document.removeEventListener("mousedown", handleClickOutside);
-  }, [changeVisibility]);
-
-  useEffect(() => {
-    if (inputRename && inputRename.current) {
-      inputRename.current.select();
-    }
-  }, [inputRename]);
-
+export const FolderBox = ({ folder, isActive, onDelete = () => null }) => {
   return (
-    <div>
-      <form
-        onSubmit={event => {
-          sdk
-            .renameFolder(folder, text)
-            .then(dispatch(renameFolder(folder.id, text)));
-          changeVisibility();
-          setText("");
-          inputRename.current.blur();
-          event.preventDefault();
-        }}
-      >
-        <input
-          ref={inputRename}
-          type="text"
-          placeholder="rename me"
-          value={text}
-          onChange={event => {
-            setText(event.target.value);
-          }}
-          autoFocus
-        />
-      </form>
-      <button
-        onClick={() => {
-          changeVisibility();
-          setText("");
-        }}
-      >
-        Cancel
-      </button>
-    </div>
+    <FolderButton folder={folder} isActive={isActive} onDelete={onDelete} />
   );
-};
-
-export const FolderBox = ({ folder, currentFolderId }) => {
-  const dispatch = useDispatch();
-
-  const [visibility, setVisibility] = useState(true);
-
-  const changeVisibility = () => setVisibility(!visibility);
-
-  if (visibility)
-    return (
-      <FolderButton
-        folder={folder}
-        dispatch={dispatch}
-        changeVisibility={changeVisibility}
-        currentFolderId={currentFolderId}
-      />
-    );
-  else
-    return (
-      <FolderInput
-        folder={folder}
-        dispatch={dispatch}
-        changeVisibility={changeVisibility}
-      />
-    );
 };
