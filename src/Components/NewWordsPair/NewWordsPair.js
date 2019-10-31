0d047d23c25297489d69750e4e6b4eda3b0492cb
reginPekin <reginbegin@gmail.com>
2019-10-31 21:56:10 +0300

    добавление новой пары слов

--- src/Components/NewWordsPair/NewWordsPair.js
+++ src/Components/NewWordsPair/NewWordsPair.js
@@ -1,10 +1,8 @@
 import React, { useState, useRef, useEffect } from "react";
 
-import { addNewWord } from "../../utils/wordUtils";
-
 import * as sdk from "../../sdk";
 
-export const NewWordsPair = ({ folderId, dispatch }) => {
+export const NewWordsPair = ({ folderId, onAdd }) => {
   const [isClicked, setIsClicked] = useState(false);
   const [foreignText, setForeignText] = useState("");
   const [nativeText, setNativeText] = useState("");
@@ -50,18 +48,19 @@ export const NewWordsPair = ({ folderId, dispatch }) => {
               <td>
                 <form
                   onSubmit={event => {
+                    event.preventDefault();
                     let newWord = {
                       folderId,
                       wordId: Math.floor(Math.random() * Math.floor(100000000)),
                       foreignWord: foreignText,
                       nativeWord: nativeText
                     };
-                    sdk.createNewWord(folderId, newWord);
-                    dispatch(addNewWord(newWord));
+                    sdk
+                      .createNewWord(folderId, newWord)
+                      .then(() => onAdd(newWord));
                     setNativeText("");
                     setForeignText("");
                     foreignInputRef.current.focus();
-                    event.preventDefault();
                   }}
                 >
                   <input
