6e076c69f221edca3449be451267d9909acdf083
reginPekin <reginbegin@gmail.com>
2019-11-01 20:04:34 +0300

    компонент кнопки

---
+++ src/Components/EditingInput/EditingInput.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect, useRef } from "react";
+
+export const EditingInput = ({
+  initialState,
+  changeVisibility = () => null,
+  onSubmit = () => null
+}) => {
+  const [text, setText] = useState(initialState);
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    if (inputRef.current && inputRef) {
+      inputRef.current.select();
+    }
+  }, []);
+
+  useEffect(() => {
+    const handleClickOutside = event => {
+      if (inputRef.current && !inputRef.current.contains(event.target)) {
+        changeVisibility();
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, [changeVisibility]);
+
+  return (
+    <form
+      onSubmit={event => {
+        onSubmit(text);
+        changeVisibility();
+        setText("");
+        event.preventDefault();
+      }}
+    >
+      <input
+        ref={inputRef}
+        type="text"
+        value={text}
+        onChange={event => setText(event.target.value)}
+        autoFocus
+      />
+    </form>
+  );
+};
---
+++ src/Components/EditingInput/index.js
@@ -0,0 +1,1 @@
+export { EditingInput } from "./EditingInput";
