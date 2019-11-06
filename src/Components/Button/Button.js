04c8fb9db77c1c9da92df7c2642f1657a3652694
reginPekin <reginbegin@gmail.com>
2019-11-06 23:27:54 +0300

    посторонний css для кнопки

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -1,10 +1,19 @@
 import React from "react";
 
+import cx from "classnames";
+
 import styles from "./Button.module.css";
 
-export const Button = ({ value, onClick = () => null }) => {
+export const Button = ({
+  value,
+  onClick = () => null,
+  buttonClassName = null
+}) => {
   return (
-    <button className={styles.button} onClick={() => onClick()}>
+    <button
+      className={cx(styles.button, buttonClassName)}
+      onClick={() => onClick()}
+    >
       {value}
     </button>
   );
