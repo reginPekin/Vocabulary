6db4010e47d48516be49d002a926d818ec3175d9
reginPekin <reginbegin@gmail.com>
2019-11-09 19:40:02 +0300

    context menu

--- src/Components/FolderBox/FolderBox.js
+++ src/Components/FolderBox/FolderBox.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-navi";
 
 import * as sdk from "../../sdk";
@@ -7,32 +7,61 @@ import styles from "./FolderBox.module.css";
 
 import { Button } from "../Button";
 
+import Menu from "../../images/menu.png";
+
 export const FolderBox = ({ folder, onDelete = () => null }) => {
+  const divRef = useRef(null);
   const [isVisible, setIsVisible] = useState(false);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const changeVisibility = () => setIsVisible(!isVisible);
+
+  useEffect(() => {
+    const handleClickOutside = event => {
+      if (divRef.current && !divRef.current.contains(event.target)) {
+        changeVisibility();
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, [changeVisibility]);
+
   return (
-    <div className={styles.boxFolder}>
-      <Link
-        href={`/voc/${folder.id}`}
-        activeClassName={styles.activeFolder}
-        className={styles.link}
-      >
-        {folder.name}
-      </Link>
-      <Button
-        buttonClassName={styles.button}
-        value=":"
-        onClick={() => setIsVisible(!isVisible)}
-      />
-      {isVisible && <div>Hi</div>}
-      {/* <Link href="/">
-        <button
-          onClick={() => {
-            sdk.deleteFolder(folder.id).then(() => onDelete(folder.id));
-          }}
-        >
-          X
-        </button>
-      </Link> */}
-    </div>
+    <>
+      {isVisible && <div className={styles.glass} />}
+      <section className={styles.boxFolder}>
+        <div className={styles.mainPart}>
+          <Link
+            href={`/voc/${folder.id}`}
+            activeClassName={styles.activeFolder}
+            className={styles.link}
+          >
+            {folder.name}
+          </Link>
+          <Button
+            buttonClassName={styles.button}
+            value={<img src={Menu} alt="Menu" height="30" />}
+            onClick={() => {
+              setIsVisible(!isVisible);
+            }}
+          />
+        </div>
+        {isVisible && (
+          <div ref={divRef} className={styles.contextMenu}>
+            <Link href="/" className={styles.deleteLink}>
+              <Button
+                buttonClassName={styles.menuButton}
+                value="Delete"
+                onClick={() => {
+                  sdk.deleteFolder(folder.id).then(() => onDelete(folder.id));
+                  changeVisibility();
+                }}
+              />
+            </Link>
+            <Button buttonClassName={styles.menuButton} value="Edit" />
+          </div>
+        )}
+      </section>
+    </>
   );
 };
