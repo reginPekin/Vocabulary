a655bf10f7a4a4880f019e4998eab0996449f1c1
reginPekin <reginbegin@gmail.com>
2019-11-16 12:46:20 +0300

    newFolder cleaning

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
+import { useNavigation } from "react-navi";
 
 import { FolderBox } from "../FolderBox";
 import { NewFolder } from "../NewFolder";
@@ -11,6 +12,8 @@ import styles from "./Menu.module.css";
 const MenuContainer = ({ beam }) => {
   const [folderNames, setFolderNames] = useState([]);
 
+  const navigation = useNavigation();
+
   useEffect(() => {
     sdk.getFolderNames().then(response => {
       setFolderNames(response);
@@ -20,7 +23,18 @@ const MenuContainer = ({ beam }) => {
   return (
     <div className={styles.folderWindow}>
       <NewFolder
-        onAdd={newFolder => setFolderNames([newFolder, ...folderNames])}
+        onAdd={text => {
+          const newFolder = {
+            name: text,
+            foreignLanguage: "Foreign language",
+            nativeLanguage: "Native language"
+          };
+          sdk.createFolder(newFolder).then(data => {
+            const newFolder = data.data[0];
+            setFolderNames([newFolder, ...folderNames]);
+            navigation.navigate(`/voc/${newFolder.id}`);
+          });
+        }}
       />
       {folderNames.map(
         (folder, key) => (
--- src/Components/NewFolder/NewFolder.js
+++ src/Components/NewFolder/NewFolder.js
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
 
-import { useNavigation } from "react-navi";
-
 import Plus from "../../images/blackPlus.png";
 
 import { Button } from "../Button";
@@ -9,49 +7,29 @@ import { EditingInput } from "../EditingInput";
 
 import styles from "./NewFolder.module.css";
 
-import * as sdk from "../../sdk";
-
 export const NewFolder = ({ onAdd }) => {
-  const navigation = useNavigation();
   const [visibility, setVisibility] = useState(true);
   const changeVisibility = () => setVisibility(!visibility);
 
-  if (visibility) {
-    return (
-      <Button
-        onClick={() => changeVisibility(!setVisibility)}
-        buttonClassName={styles.newFolderButtom}
-      >
-        <section className={styles.span}>
-          <img src={Plus} alt="Plus" width="15" />
-          <span> Add folder </span>
-        </section>
-      </Button>
-    );
-  } else {
+  if (!visibility) {
     return (
       <EditingInput
         formClassName={styles.form}
         inputClassName={styles.newFolderInput}
         changeVisibility={changeVisibility}
-        onSubmit={text => {
-          const newFolder = {
-            name: text,
-            date: Date.now(),
-            id: Math.floor(Math.random() * Math.floor(100000000)),
-            words: [],
-            foreignLanguage: "Foreign language",
-            nativeLanguage: "Native language"
-          };
-
-          sdk
-            .createFolder(newFolder)
-            .then(onAdd(newFolder))
-            .then(() => {
-              navigation.navigate(`/voc/${newFolder.id}`);
-            });
-        }}
+        onSubmit={text => onAdd(text)}
       />
     );
   }
+  return (
+    <Button
+      onClick={() => changeVisibility(!setVisibility)}
+      buttonClassName={styles.newFolderButtom}
+    >
+      <section className={styles.span}>
+        <img src={Plus} alt="Plus" width={15} />
+        <span> Add folder </span>
+      </section>
+    </Button>
+  );
 };
