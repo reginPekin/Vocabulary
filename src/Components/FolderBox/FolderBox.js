90171df8d54bc8bbc63f1e3f2bfd937e535ecea0
reginPekin <reginbegin@gmail.com>
2019-11-18 17:58:34 +0300

    + popup

--- src/Components/FolderBox/FolderBox.js
+++ src/Components/FolderBox/FolderBox.js
@@ -1,20 +1,16 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import { Link } from "react-navi";
-
-import { useOnClickOutside } from "../../utils/hooks";
 
 import styles from "./FolderBox.module.css";
 
 import { Button } from "../Button";
+import { Popup } from "../Popup";
 
 import Menu from "../../images/menu.svg";
 
 export const FolderBox = ({ folder, onDelete = () => null }) => {
-  const divRef = useRef(null);
   const [isVisible, setIsVisible] = useState(false);
   const changeVisibility = () => setIsVisible(!isVisible);
-
-  useOnClickOutside(divRef, () => changeVisibility());
 
   return (
     <section className={styles.boxFolder}>
@@ -35,23 +31,24 @@ export const FolderBox = ({ folder, onDelete = () => null }) => {
           <img src={Menu} alt="Menu" width={27} />
         </Button>
       </div>
-      {isVisible && <div className={styles.glass} />}
-      {isVisible && (
-        <div ref={divRef} className={styles.contextMenu}>
-          <Link href="/" className={styles.deleteLink}>
-            <Button
-              buttonClassName={styles.menuButton}
-              onClick={() => {
-                onDelete();
-                changeVisibility();
-              }}
-            >
-              Delete
-            </Button>
-          </Link>
-          <Button buttonClassName={styles.menuButton}>Edit</Button>
-        </div>
-      )}
+      <Popup
+        positionClassName={styles.popup}
+        isVisible={isVisible}
+        changeVisibility={() => changeVisibility()}
+      >
+        <Link href="/" className={styles.deleteLink}>
+          <Button
+            buttonClassName={styles.menuButton}
+            onClick={() => {
+              onDelete();
+              changeVisibility();
+            }}
+          >
+            Delete
+          </Button>
+        </Link>
+        <Button buttonClassName={styles.menuButton}>Edit</Button>
+      </Popup>
     </section>
   );
 };
