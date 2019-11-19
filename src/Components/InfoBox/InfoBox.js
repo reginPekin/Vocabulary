a84ec86e4f768b8fd1ee8700d4878190617810f7
reginPekin <reginbegin@gmail.com>
2019-11-19 05:28:19 +0300

    shorter

--- src/Components/InfoBox/InfoBox.js
+++ src/Components/InfoBox/InfoBox.js
@@ -2,28 +2,19 @@ import React, { useState } from "react";
 
 import styles from "./InfoBox.module.css";
 
-import { Button } from "../Button";
-import { EditingInput } from "../EditingInput";
+import { InputButton } from "../InputButton";
 
 export const InfoBox = ({ onRename, name }) => {
   const [visibility, setVisibility] = useState(true);
   const changeVisibility = () => setVisibility(!visibility);
-
-  if (!visibility)
-    return (
-      <EditingInput
-        inputClassName={styles.inputClassName}
-        value={name}
-        changeVisibility={changeVisibility}
-        onSubmit={value => onRename(value)}
-      />
-    );
   return (
-    <Button
-      onClick={() => changeVisibility()}
+    <InputButton
+      visibility={visibility}
+      changeVisibility={() => changeVisibility()}
+      onChange={value => onRename(value)}
+      text={name}
+      inputClassName={styles.inputClassName}
       buttonClassName={styles.buttonClassName}
-    >
-      {name}
-    </Button>
+    />
   );
 };
