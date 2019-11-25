80dc79e7ced78da0e025aab298b99f4b9cf758fc
reginPekin <reginbegin@gmail.com>
2019-11-25 20:53:55 +0300

    sort + infoBox css

--- src/Components/InfoBox/InfoBox.js
+++ src/Components/InfoBox/InfoBox.js
@@ -1,20 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 import styles from "./InfoBox.module.css";
 
 import { InputButton } from "../InputButton";
 
-export const InfoBox = ({ onRename, name }) => {
+export const InfoBox = ({ onRename, name, onSort }) => {
+  const sortRef = useRef(null);
   const [visibility, setVisibility] = useState(true);
   const changeVisibility = () => setVisibility(!visibility);
   return (
-    <InputButton
-      visibility={visibility}
-      changeVisibility={() => changeVisibility()}
-      onChange={value => onRename(value)}
-      text={name}
-      inputClassName={styles.inputClassName}
-      buttonClassName={styles.buttonClassName}
-    />
+    <div className={styles.infoBox}>
+      <InputButton
+        visibility={visibility}
+        changeVisibility={() => changeVisibility()}
+        onChange={value => onRename(value)}
+        text={name}
+        inputClassName={styles.inputClassName}
+        formClassName={styles.formClassName}
+        buttonClassName={styles.buttonClassName}
+      />
+      <form
+        onSubmit={event => {
+          event.preventDefault();
+          onSort();
+        }}
+      >
+        <label className={styles.sort}>
+          Sort by:
+          <select name="select" ref={sortRef}>
+            <option value="data">Data</option>
+            <option value="name">Name</option>
+            <option value="speechPart">Speech parts</option>
+          </select>
+        </label>
+        <input type="submit" value="Submit" style={{ visibility: "hidden" }} />
+      </form>
+    </div>
   );
 };
