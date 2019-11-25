95c5a65f5ba920344c71ec426507a6c3005c9a17
reginPekin <reginbegin@gmail.com>
2019-11-26 02:31:14 +0300

    + react-select

--- src/Components/InfoBox/InfoBox.js
+++ src/Components/InfoBox/InfoBox.js
@@ -1,13 +1,38 @@
 import React, { useState, useRef } from "react";
+import Select from "react-select";
 
 import styles from "./InfoBox.module.css";
 
 import { InputButton } from "../InputButton";
 
-export const InfoBox = ({ onRename, name, onSort }) => {
+export const InfoBox = ({ onRename, name, onSort, sortMethod }) => {
   const sortRef = useRef(null);
   const [visibility, setVisibility] = useState(true);
   const changeVisibility = () => setVisibility(!visibility);
+  let defaultValue = "";
+  switch (sortMethod) {
+    case "date":
+      defaultValue = "Date";
+      break;
+    case "foreignWords":
+      defaultValue = "Foreign words";
+      break;
+    case "nativeWords":
+      defaultValue = "Native words";
+      break;
+    case "speechPart":
+      defaultValue = "Speech parts";
+      break;
+    default:
+      console.log("none");
+  }
+
+  const options = [
+    { value: "date", label: "Date" },
+    { value: "foreignWords", label: "Foreign words" },
+    { value: "nativeWords", label: "Native words" },
+    { value: "speechPart", label: "Speech parts" }
+  ];
   return (
     <div className={styles.infoBox}>
       <InputButton
@@ -27,12 +52,14 @@ export const InfoBox = ({ onRename, name, onSort }) => {
       >
         <label className={styles.sort}>
           Sort by:
-          <select name="select" ref={sortRef}>
-            <option value="date">Date</option>
-            <option value="foreignWords">Foreign words</option>
-            <option value="nativeWords">Native words</option>
-            <option value="speechPart">Speech parts</option>
-          </select>
+          <Select
+            ref={sortRef}
+            options={options}
+            onChange={event => {
+              onSort(event.value);
+            }}
+            defaultValue={{ label: defaultValue }}
+          />
         </label>
         <input type="submit" value="Submit" style={{ visibility: "hidden" }} />
       </form>
