3f5f24e217f4839e3ec9414377984cb6d92c31de
reginPekin <reginbegin@gmail.com>
2019-12-10 18:52:55 +0300

    small changes

--- public/Main.js
+++ public/Main.js
@@ -1,6 +1,6 @@
 const { app, BrowserWindow } = require("electron");
 const createWindow = () => {
-  const win = new BrowserWindow({ width: 1000, height: 600 });
+  const win = new BrowserWindow({ width: 900, height: 700 });
   win.loadURL("http://localhost:3000/");
 };
 
