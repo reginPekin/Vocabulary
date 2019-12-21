0820895af311ef397cee396381fc39f86acd0acb
reginPekin <reginbegin@gmail.com>
2019-12-21 22:13:08 +0300

    common part

--- src/sdk/index.js
+++ src/sdk/index.js
@@ -1,24 +1,23 @@
 import axios from "axios";
 
+const API_ENDPOINT = "http://localhost:4000/vocabulary";
+
 export const getFolderNames = async () => {
   return await axios
-    .get("http://localhost:4000/vocabulary/folders/names")
+    .get(`${API_ENDPOINT}/folders/names`)
     .then(response => response.data);
 };
 
 export const editWord = async newName => {
   return await axios.patch(
-    "http://localhost:4000/vocabulary/folders/" +
-      newName.id +
-      "/words/edit/" +
-      newName.wordId,
+    `${API_ENDPOINT}/folders/` + newName.id + "/words/edit/" + newName.wordId,
     newName
   );
 };
 
 export const editSpeechPart = async newSpeechPart => {
   return await axios.patch(
-    "http://localhost:4000/vocabulary/folder/" +
+    `${API_ENDPOINT}/folder/` +
       newSpeechPart.id +
       "/words/" +
       newSpeechPart.wordId +
@@ -28,32 +27,29 @@ export const editSpeechPart = async newSpeechPart => {
 };
 
 export const deleteFolder = async id => {
-  return await axios.delete("http://localhost:4000/vocabulary/folders/" + id);
+  return await axios.delete(`${API_ENDPOINT}/folders/` + id);
 };
 
 export const renameFolder = async (id, text) => {
-  return await axios.patch("http://localhost:4000/vocabulary/folders/" + id, {
+  return await axios.patch(`${API_ENDPOINT}/folders/` + id, {
     name: text
   });
 };
 
 export const createNewWord = async newWord => {
   return await axios.post(
-    "http://localhost:4000/vocabulary/folders/" + newWord.folderId + "/words",
+    `${API_ENDPOINT}/folders/` + newWord.folderId + "/words",
     newWord
   );
 };
 
 export const createFolder = async newFolder => {
-  return await axios.post(
-    "http://localhost:4000/vocabulary/folders",
-    newFolder
-  );
+  return await axios.post(`${API_ENDPOINT}/folders`, newFolder);
 };
 
 export const deleteWordsPair = async (id, wordId) => {
   return await axios.post(
-    "http://localhost:4000/vocabulary/folders/" + id + "/words/" + wordId,
+    `${API_ENDPOINT}/folders/` + id + "/words/" + wordId,
     { id, wordId }
   );
 };
@@ -61,7 +57,7 @@ export const deleteWordsPair = async (id, wordId) => {
 export const getWordsArray = async (folderId, sort, sortDirecton) => {
   return await axios
     .get(
-      "http://localhost:4000/vocabulary/folders/" +
+      `${API_ENDPOINT}/folders/` +
         folderId +
         "/words?sort=" +
         sort +
@@ -73,22 +69,19 @@ export const getWordsArray = async (folderId, sort, sortDirecton) => {
 
 export const getFolder = async folderId => {
   return await axios
-    .get("http://localhost:4000/vocabulary/folders/" + folderId)
+    .get(`${API_ENDPOINT}/folders/` + folderId)
     .then(response => response.data);
 };
 
 export const changeLanguage = async (id, text, language) => {
-  return await axios.patch(
-    "http://localhost:4000/vocabulary/folders/" + id + "/language",
-    {
-      language,
-      renamedLanguage: text
-    }
-  );
+  return await axios.patch(`${API_ENDPOINT}/folders/` + id + "/language", {
+    language,
+    renamedLanguage: text
+  });
 };
 
 export const sortNext = async (arr, sortMethod) => {
-  return await axios.post("http://localhost:4000/vocabulary/folders/sort", {
+  return await axios.post(`${API_ENDPOINT}/folders/sort`, {
     arr,
     sortMethod
   });
